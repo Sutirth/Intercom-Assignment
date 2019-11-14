8f51ecbc24c317886ed274dc9f7c9317253c610e
Sutirth Chakravarty <schakravarty@equalexperts.com>
2019-11-14 12:16:53 +0530

    first commit

---
+++ app/src/main/java/constraint/com/intercomandroidassigment/MainActivity.kt
@@ -0,0 +1,12 @@
+package constraint.com.intercomandroidassigment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
